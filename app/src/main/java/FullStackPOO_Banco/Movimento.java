6a8246a371f2852b12ff2cfd7b7f8da80f6b84ca
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2022-09-01 15:58:09 -0300

    Create Movimento Class

---
+++ app/src/main/java/FullStackPOO_Banco/Movimento.java
@@ -0,0 +1,40 @@
+package FullStackPOO_Banco;
+
+/**
+ *
+ * @author RC_Ve
+ */
+public class Movimento {
+
+// //atributos 
+    private String tipoMovimento;  // posso fazer um Enum
+    private float valor;
+
+    // construtores
+    public Movimento() {
+    }
+
+    public Movimento(String tipoMovimento, float valor) {
+        this.tipoMovimento = tipoMovimento;
+        this.valor = valor;
+
+    }
+
+    // funcoes padroes
+    public String getTipoMovimento() {
+        return tipoMovimento;
+    }
+
+    public void setTipoMovimento(String tipoMovimento) {
+        this.tipoMovimento = tipoMovimento;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+}
